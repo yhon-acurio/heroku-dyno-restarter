8026a22a66037d05aefa0c5d1ecfb9736af70367
Artem Vasiliev <artem.job@gmail.com>
2016-06-27 20:56:33 +0300

    chore: some logging

--- app/jobs/restart_app_job.rb
+++ app/jobs/restart_app_job.rb
@@ -13,13 +13,17 @@ class RestartAppJob < ApplicationJob
     end
 
     def self.dynos
-      connection.dyno.list(ENV['APP_NAME']).map do |dyno_info|
+      result = connection.dyno.list(ENV['APP_NAME']).map do |dyno_info|
         Dyno.new(dyno_info)
       end
+      Rails.logger.debug("dynos: #{dynos.inspect}")
+      result
     end
 
-    def self.running_web_dynos
-      dynos.select { |dyno| dyno.worker? && dyno.up? }
+    def self.running_worker_dynos
+      result = dynos.select { |dyno| dyno.worker? && dyno.up? }
+      Rails.logger.debug("running_web_dynos: #{running_worker_dynos.inspect}")
+      result
     end
 
     def worker?
@@ -35,7 +39,9 @@ class RestartAppJob < ApplicationJob
     end
 
     def restart!
-      connection.dyno.restart(ENV['APP_NAME'], name)
+      result = connection.dyno.restart(ENV['APP_NAME'], name)
+      Rails.logger.debug("restart! result: #{result.inspect}")
+      result
     end
 
     def initialize(info)
@@ -47,7 +53,8 @@ class RestartAppJob < ApplicationJob
 
   def perform(*args)
     if Dyno.connection
-      Dyno.running_web_dynos.each do |dyno|
+      Dyno.running_worker_dynos.each do |dyno|
+        Rails.logger.debug("running_web_dynos: #{running_worker_dynos.inspect}")
         dyno.restart!
       end
     end
