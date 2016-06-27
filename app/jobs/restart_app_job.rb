6b20c922a0038709717e13326f73f0fde40cf78f
Artem Vasiliev <artem.job@gmail.com>
2016-06-27 21:14:52 +0300

    chore: corrected logging

--- app/jobs/restart_app_job.rb
+++ app/jobs/restart_app_job.rb
@@ -1,28 +1,30 @@
 class RestartAppJob < ApplicationJob
-  queue_as :restarts
+  queue_as :default
 
   class Dyno
     attr_accessor :type
     attr_accessor :name
     attr_accessor :state
 
-    def self.connection
-      if ENV['RESTART_API_KEY']
-        @@connection ||= PlatformAPI.connect_oauth(ENV['RESTART_API_KEY'])
+    def self.connection1
+      result = if ENV['RESTART_API_KEY']
+        @@connection1 ||= PlatformAPI.connect_oauth(ENV['RESTART_API_KEY'])
       end
+      Rails.logger.debug("connection1: #{result.inspect}")
+      result
     end
 
     def self.dynos
-      result = connection.dyno.list(ENV['APP_NAME']).map do |dyno_info|
+      result = connection1.dyno.list(ENV['APP_NAME']).map do |dyno_info|
         Dyno.new(dyno_info)
       end
-      Rails.logger.debug("dynos: #{dynos.inspect}")
+      Rails.logger.debug("dynos: #{result.inspect}")
       result
     end
 
     def self.running_worker_dynos
       result = dynos.select { |dyno| dyno.worker? && dyno.up? }
-      Rails.logger.debug("running_web_dynos: #{running_worker_dynos.inspect}")
+      Rails.logger.debug("running_web_dynos: #{result.inspect}")
       result
     end
 
@@ -34,12 +36,12 @@ class RestartAppJob < ApplicationJob
       state == 'up'
     end
 
-    def connection
-      self.class.connection
+    def connection1
+      self.class.connection1
     end
 
     def restart!
-      result = connection.dyno.restart(ENV['APP_NAME'], name)
+      result = connection1.dyno.restart(ENV['APP_NAME'], name)
       Rails.logger.debug("restart! result: #{result.inspect}")
       result
     end
@@ -52,9 +54,9 @@ class RestartAppJob < ApplicationJob
   end
 
   def perform(*args)
-    if Dyno.connection
+    if Dyno.connection1
       Dyno.running_worker_dynos.each do |dyno|
-        Rails.logger.debug("running_web_dynos: #{running_worker_dynos.inspect}")
+        Rails.logger.debug("restarting dyno: #{dyno.inspect}")
         dyno.restart!
       end
     end
